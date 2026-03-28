649e8be9c2bd06e59cbc9578dcf5eaa47cf7da7b
yuna-espejo <y.espejo.santana@gmail.com>
2026-03-28 21:27:51 +0100

    add analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import "./globals.css";
 import { Inter } from "next/font/google";
 import Navbar from "./components/Navbar";
 import ContactButton from "./components/ContactButton";
+import { Analytics } from "@vercel/analytics/next";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -21,6 +22,7 @@ export default function RootLayout({
         <Navbar />
         <ContactButton />
         {children}
+        <Analytics />
       </body>
     </html>
   );
